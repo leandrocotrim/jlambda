2187f9711558aced6545ee3fb8d65156ec30a7d9
Leandro Cotrim <dclwebsystems@gmail.com>
2016-09-21 01:54:45 -0300

    version 1.0 with debug and minify

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var version = '-1.0';
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+ 
+gulp.task('compress', function() {
+  gulp.src('jlambda.js')
+    .pipe(minify({
+        ext:{
+            src: version + '.js',
+            min: version + '.min.js'
+        },
+        exclude: ['tasks']//,
+        //ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
